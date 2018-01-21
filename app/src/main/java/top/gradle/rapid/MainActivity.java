c703587a0c0c8b990b58f5cf3f86eed8603d4849
unknown <lingrixin@live.cn>
2018-01-21 12:01:25 +0800

    初始化项目

---
+++ app/src/main/java/top/gradle/rapid/MainActivity.java
@@ -0,0 +1,18 @@
+package top.gradle.rapid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by 日新 on 2018/1/21.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
